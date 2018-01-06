1cda6a60ceebc1fce6a189485b795f9d9bab7693
Katelyn P <kphan@ucvts.tec.nj.us>
2018-01-06 01:08:13 +0000

    Created character lass with player attributes and combat functions.

--- master_game_file.py
+++ master_game_file.py
@@ -30,13 +30,20 @@ class Character():
         self.classed = None
         self.weapon = None
 
-player = Character("Tom")
+    #Combat Manager
+    def get_atk(self):
+        """How much damage is delt"""
+        atk = random.randint(4, self.ap)
+        return atk
+    def take_dmg(self, dmg):
+        """How much damage is taken"""
+        self.hp = self.hp - dmg
+    def rnd_attack(self):
+        """Randomizes a choice between attack and defend for attacker (CPU)"""
+        atk = random.choice(["attack", "defend"])
+        return atk
 
-player.inventory["silver"]=2
-player.inventory["gold"]=3
 
-for k, v in player.inventory.items():
-    print(k, ":", v).sort()
 
 
 
